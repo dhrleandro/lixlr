149e65c7848d81028a5a2213ba1bc5bf4022fe6c
Leandro Daher <117833992+dhrleandro@users.noreply.github.com>
2022-12-19 23:11:00 -0300

    themeswicther state in local storage

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -14,8 +14,40 @@ interface ThemeProviderProps {
 
 function ThemeProvider(props: ThemeProviderProps) {
 
-  const  [toggle, setToggle] = React.useState(false);
-  const toggleFunction = () => setToggle(!toggle);
+  const THEME_STATE = 'theme_state';
+
+  function setLocalStorageState(bool: boolean): void {
+    try {
+      const value = bool ? '1' : '0';
+      window.localStorage.setItem(THEME_STATE, value);
+    } catch (error) {
+      return;
+    }
+  }
+
+  function getLocalStorageState(): boolean {
+    try {
+      const value = window.localStorage.getItem(THEME_STATE);
+      if (!value)
+        return false;
+
+      const parse = Math.floor(parseInt(value));
+      if (parse < 0 || parse > 1)
+        return false;
+
+      return parse === 1 ? true : false;
+
+    } catch (error) {
+      return false;
+    }
+  }
+
+  const  [toggle, setToggle] = React.useState(getLocalStorageState());
+  const toggleFunction = () => {
+    const value = !toggle;
+    setToggle(value);
+    setLocalStorageState(value);
+  }
 
   const themeContext = {
     toggle,
