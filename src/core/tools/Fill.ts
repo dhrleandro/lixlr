c060795b88f336b7ad3b0280fa3745782c64fa6b
Leandro Daher <117833992+dhrleandro@users.noreply.github.com>
2022-12-19 23:11:00 -0300

    [core] fixing flood fill tool

--- src/core/tools/Fill.ts
+++ src/core/tools/Fill.ts
@@ -8,29 +8,20 @@ export default class Fill extends BaseTool {
 
   public readonly type = ToolType.FILL;
   public readonly cursorCss: string = 'crosshair';
-  private lastPoint: Point2D | undefined;
   private color: RGBA;
 
   constructor() {
     super();
-
-    this.lastPoint = Point2D.create(0,0);
     this.color = RGBA.create(0,0,0);
-
     this.addColorProperty('color', RGBA.create(0,0,0));
   }
 
   public onPointerDown(point: Point2D, context: CanvasRenderingContext2D): void {
-    this.lastPoint = point;
     this.color = this.getProperty('color')?.value as RGBA;
+    floodFill(context, point.x, point.y, this.color);
   }
 
-  public onPointerUp(point: Point2D, context: CanvasRenderingContext2D): void {
-    if (this.lastPoint)
-      floodFill(context, this.lastPoint.x, this.lastPoint.y, this.color);
-
-    this.lastPoint = undefined;
-  }
+  public onPointerUp(point: Point2D, context: CanvasRenderingContext2D): void {}
 
   public onPointerMove(point: Point2D, context: CanvasRenderingContext2D): void {}
 }
--- src/core/utils/floodfill.ts
+++ src/core/utils/floodfill.ts
@@ -6,50 +6,53 @@ import { getPixel } from "./graphic";
 function validCoordinates(matrix: Uint8ClampedArray, imageWidth: number, x: number, y: number): boolean {
   const red = y * (imageWidth * 4) + x * 4;
 
-  return (
-    matrix[red] !== undefined &&
-    matrix[red + 1] !== undefined &&
-    matrix[red + 2] !== undefined &&
-    matrix[red + 3] !== undefined
-  );
+  return matrix[red] !== undefined;
 }
 
 // Set a cell in the matrix
 function setCell(matrix: Uint8ClampedArray, imageWidth: number, x: number, y: number, color: RGBA): void {
-  if (validCoordinates(matrix, imageWidth, x, y)) {
+  //if (validCoordinates(matrix, imageWidth, x, y)) {
     const red = y * (imageWidth * 4) + x * 4;
 
     matrix[red] = color.red;
     matrix[red + 1] = color.green;
     matrix[red + 2] = color.blue;
     matrix[red + 3] = color.alpha;
-  }
+  //}
 }
 
 export function getMatrixPixel(matrix: Uint8ClampedArray, imageWidth: number, x: number, y: number): RGBA {
   const red = y * (imageWidth * 4) + x * 4;
-  return RGBA.create(matrix[red], matrix[red + 1], matrix[red + 2], matrix[red + 3]);
+  const rgba = RGBA.create(matrix[red], matrix[red + 1], matrix[red + 2], matrix[red + 3]);
+
+  console.log(x, y, matrix[red], matrix[red + 1], matrix[red + 2], matrix[red + 3]);
+  return rgba;
 }
 
 // Flood fill algorithm implemented with a stack on the heap
 // This algorithm will also work with big size matrixes
 
-export default function floodFill(context: CanvasRenderingContext2D, row: number, col: number, color: RGBA): void {
+export default function floodFill(context: CanvasRenderingContext2D, row: number, col: number, color: RGBA) {
   const matrix = context.getImageData(0, 0, context.canvas.width, context.canvas.height);
+  console.log(matrix.data.slice(0, 16));
   let matrixData = matrix.data; // reference of imageData.data
-
+  context.putImageData(matrix, 0, 0);
   row = Math.floor(row);
   col = Math.floor(col);
 
-  const delimitedColor = getPixel(matrix, col, row);
+  const delimitedColor = getMatrixPixel(matrix.data, matrix.width, row, col);
 
-  let fillStack: {row: number, col: number}[] = [];
+  let fillStack: any = [];
   fillStack.push({row, col});
 
   while (fillStack.length > 0) {
     const point = fillStack.pop();
-    const row = point?.row || -1;
-    const col = point?.col || -1;
+
+    if (!point)
+      continue;
+
+    const row = point.row;
+    const col = point.col;
 
     if (!validCoordinates(matrixData, matrix.width, row, col))
       continue;
@@ -63,6 +66,7 @@ export default function floodFill(context: CanvasRenderingContext2D, row: number
     fillStack.push({row: row - 1, col: col});
     fillStack.push({row: row, col: col + 1});
     fillStack.push({row: row, col: col - 1});
+
   }
 
   context.putImageData(matrix, 0, 0);
