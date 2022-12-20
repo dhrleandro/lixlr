2446d477e499f64888e0de6abd40eb1ba5e832f9
Leandro Daher <117833992+dhrleandro@users.noreply.github.com>
2022-12-19 23:11:00 -0300

    remove editorconfig vscode extension

---
+++ src/core/View.ts
@@ -0,0 +1,16 @@
+export default abstract class View {
+
+    private child: View | undefined;
+    private canvasReference: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D;
+
+    constructor(canvasReference: HTMLCanvasElement) {
+
+    }
+
+    /**
+     * @param d
+     */
+    public teste() { }
+
+}
